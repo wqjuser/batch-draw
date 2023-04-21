f8bc0cfcc65626d1d3c790948e17876a33d5637b
wqj <wqjuser@gmail.com>
2023-04-22 07:21:07 +0800

    尝试失败

--- scripts/batch_draw_i2i.py
+++ scripts/batch_draw_i2i.py
@@ -378,13 +378,8 @@ def add_watermark(need_add_watermark_images, need_add_watermark_images1, new_ima
             original_dir, original_filename = os.path.split(need_add_watermark_images[i][0])
 
         watermarked_path = os.path.join(original_dir, f"watermarked_{original_filename}")
-        # 这样保存会没有任何生成图片的信息
-        # watermarked_image.save(watermarked_path)
-        # 试试这样保存序号会乱不
-        (watered_path, _) = images.save_image(watermarked_image, original_dir, "",
-                                              forced_filename=f"watermarked_{original_filename}",
-                                              prompt=p.prompt_for_display, seed=processed.seed, grid=False, p=p)
-        img1 = Image.open(watered_path)
+        watermarked_image.save(watermarked_path)
+        img1 = Image.open(watermarked_path)
         watered_images.append(img1)
     if int(text_watermark_target) == 2:
         for i, img in enumerate(need_add_watermark_images1):
