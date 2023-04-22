217f93157160ff2f4b742275fa6231a96a5214f0
wqj <wqjuser@gmail.com>
2023-04-22 16:52:29 +0800

    打印文件名以及文件的编码格式

--- scripts/batch_draw_t2i.py
+++ scripts/batch_draw_t2i.py
@@ -183,8 +183,9 @@ def mcprocess(p, prompt_txt, file_txt, jump, use_individual_prompts, prompts_fol
 
         if file_idx < len(prompt_files):
             prompt_file = os.path.join(prompts_folder, prompt_files[file_idx])
-            print("文件是:", prompt_file)
-            # 打开文件，获取文件编码
+            if not prompt_file.endswith(".txt"):
+                return
+                # 打开文件，获取文件编码
             with open(prompt_file, "rb") as f:
                 result = chardet.detect(f.read())
                 file_encoding = result['encoding']
